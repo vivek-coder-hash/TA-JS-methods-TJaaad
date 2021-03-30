2e98d0d83a385747dc3688c721c5d2c250035bd3
vivek sharma <vivekslim@gmail.com>
2021-03-30 12:31:44 +0530

    strings

--- block-BJaadc/code/index.js
+++ block-BJaadc/code/index.js
@@ -1,28 +1,40 @@
 let message = 'If you are still thinking the answer is no!';
 
 // Log the length of the variable message like `The length of [message] is [length]`
+console.log(`the length of ${message} is ${message.length}`)
 
 // Log the character at index 0 of the variable message like `The first character of [message] is [character]`
+console.log(`The first character of ${message} is ${message.charAt(0)}`)
 
 // Log the last character of message using length like `The last character of [message] is [character]`
+console.log(`The last character of ${message} is ${message.charAt(message.length-1)}`)
 
 // Log the index of word "you" in message
+console.log(message.indexOf("you"))
 
 // Log the index of word "still" in message
+console.log(message.indexOf("still"))
 
 // Log the index of word "answers" in message
+console.log(message.indexOf("answers"))
 
 // Log true or false based on whether the word "answers" exist in message or not
+console.log(message.includes("answers"))
 
 // Log true or false based on whether the word "they" exist in message or not
+console.log(message.includes("they"))
 
 // Log true or false based on whether the word "is" exist in message or not
+console.log(message.includes("is"))
 
 // Log true or false based on whether the word "Is" exist in message or not
+console.log(message.includes("IS"))
 
 // Check and log whether the word from index 3 to 6 is "you" or not (use slice)
+console.log(message.slice(3,6))
 
 // Check and log whether the word from index 7 to 8 is "a" or not
+console.log(message.slice(7,8))
 
 // Check and log whether the last 3 character in message is "no!" or not (you can use -3 in slice)
 
